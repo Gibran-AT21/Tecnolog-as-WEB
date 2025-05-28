4fc0ea5abe32c6f8d026c8d6372cac562fbcfb3d
Gibrán Alvarado Tovar <323030006@upjr.edu.mx>
2025-05-28 07:59:08 -0600

    Delete Proyectos/genericos directory

--- Proyectos/genericos/genericos2.ts
+++
@@ -1,9 +0,0 @@
-export function display(valor: number): number {
-  console.log(valor);
-  return valor;
-}
-
-export function displayString(valor: string): string {
-  console.log(valor);
-  return valor;
-}
